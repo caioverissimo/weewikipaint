2f1a1ca46a4938d8f2c984a6af69b48715c8d9bc
caioverissimo <caio.veb@gmail.com>
2015-12-16 21:29:00 -0200

    Installed nodeunit

--- Jakefile.js
+++ Jakefile.js
@@ -3,33 +3,7 @@
 "use strict";
 
 desc("Build and test");
-task("default", ["lint"]);
-
-desc("Lint everything");
-task("lint", [], function() {
-	var lint = require("./build/lint/lint_runner.js");
-
-	var files = new jake.FileList();
-	files.include("**/*.js");
-	files.exclude("node_modules");
-
-	var passed = lint.validateFileList(files.toArray(), nodeLintOptions(), {});
-	if(!passed) fail("Lint failed");
-});
-
-desc("Integrate");
-task("integrate", ["default"], function() {
-	console.log("1. Make sure 'git status' is clean.");
-	console.log("2. Build on the integration box.");
-	console.log("	a. Walk over to integration box.");
-	console.log("	b. 'git pull'");
-	console.log("	c. 'jake'");
-	console.log("	d. If jake fails, stop! Try again after fixing the issue.");
-	console.log("3. 'git checkout integration'");
-	console.log("4. 'git merge master --no-ff --log'");
-	console.log("5. 'git checkout master'");
-});
-
+task("default", ["lint","test"]);
 var nodeLintOptions = function() {
 	return {
 		bitwise: true,
@@ -49,4 +23,35 @@ var nodeLintOptions = function() {
 		node: true
 	};
 };
+desc("Lint everything");
+task("lint", [], function() {
+	var lint = require("./build/lint/lint_runner.js");
+
+	var files = new jake.FileList();
+	files.include("**/*.js");
+	files.exclude("node_modules");
+
+	var passed = lint.validateFileList(files.toArray(), nodeLintOptions(), {});
+	if(!passed) fail("Lint failed");
+});
+
+desc("Test everything");
+task("test", [], function(){
+	console.log("test goes here");
+});
+
+desc("Integrate");
+task("integrate", ["default"], function() {
+	console.log("1. Make sure 'git status' is clean.");
+	console.log("2. Build on the integration box.");
+	console.log("	a. Walk over to integration box.");
+	console.log("	b. 'git pull'");
+	console.log("	c. 'jake'");
+	console.log("	d. If jake fails, stop! Try again after fixing the issue.");
+	console.log("3. 'git checkout integration'");
+	console.log("4. 'git merge master --no-ff --log'");
+	console.log("5. 'git checkout master'");
+});
+
+
 }());
--- src/server/server.js
+++ src/server/server.js
@@ -1,3 +1,5 @@
 // Copyright (c) 2012 TItanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
-
-console.log("Hello. I`m a server. No, really. What?");
+(function() {
+	"use strict";
+	console.log("Hello. I`m a server. No, really. What?");
+}());
