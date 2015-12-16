3038446338c371a1292fbbb83180fe0ef62cdc47
Caio Verissimo <caio.veb@gmail.com>
2015-12-16 11:32:16 -0200

    adding options to lint

--- Jakefile.js
+++ Jakefile.js
@@ -13,6 +13,20 @@ task("lint", [], function() {
 	files.exclude("node_modules");
 
 	var options = {
+		bitwise: true,
+		curly: false,
+		eqeqeq: true,
+		forin: true,
+		immed: true,
+		latedef: true,
+		newcap: true,
+		noarg: true,
+		noempty: true,
+		nonew: true,
+		regexp: true,
+		undef: true,
+		strict: true,
+		trailing: true,
 		node: true
 	};
 
